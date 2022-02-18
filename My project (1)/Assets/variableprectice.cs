5c60337f0a6c48732a4f09422346e317cb65675c
Sophie Cottle <sophiecottle15@gmail.com>
2022-02-17 20:17:22 -0700

    fun added to variables

--- My project (1)/Assets/variableprectice.cs
+++ My project (1)/Assets/variableprectice.cs
@@ -19,7 +19,13 @@ public class variableprectice : MonoBehaviour
         string mood = "tired"
         
 string character_name = "Spacebob";
+int characterAge = 21
+float amount_of_cats = .7
+string fun = "I love this!"
 Console.WriteLine("In a galaxy not too far away there was " + character_name);
+Console.WriteLine(string(character_name), "is", int(characterAge), "years old");
+Console.WriteLine(string(character_name), "has", float(amount_of_cats), "cats");
+Console.WriteLine(string(fun));
 
 
             
