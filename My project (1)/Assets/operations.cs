fbfa173442cbd8c98e580b9e18aa532a45105fe2
Sophie Cottle <sophiecottle15@gmail.com>
2022-02-17 19:52:15 -0700

    added operations

---
+++ My project (1)/Assets/operations.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class operations : MonoBehaviour
+{
+    private int sum1 = 4 + 3; 
+    int sum2 = sum1 + 34
+    int sum3 = sum2 + 45
+        
+int x = 10
+    x + 4
+        
+        int x = 12
+ int y = x + 14
+int z = y * 2
+int a = 3
+int b = 4
+int c = a / b
+int x <= 100
+int s == 56
+int t != 45
+
+       
+            
+
+}
