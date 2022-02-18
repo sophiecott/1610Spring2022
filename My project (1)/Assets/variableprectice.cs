1e0396244019fb57fc032b94cfa91271ef559338
Sophie Cottle <sophiecottle15@gmail.com>
2022-02-17 19:33:26 -0700

    added variables

---
+++ My project (1)/Assets/variableprectice.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variableprectice : MonoBehaviour
+{
+    int myNum = 34
+        
+        string myName = "Sophie"
+            float decimal = 3.44
+        string food = milkshake
+        float miles_per_gallon = 14.6
+        int age = 21
+        long grandpas_age = 40000000000
+        double = 124E4D
+        
+            
+        int tip_amount = 17
+        string mood = "tired"
+        
+            
+}
