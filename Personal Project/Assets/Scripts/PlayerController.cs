a269ceceddc267d44f41baf8daef0d72864545b0
Sophie Cottle <sophiecottle15@gmail.com>
2022-04-12 16:24:28 -0600

    personal project and prototype 5
    both adds and changes to both projects

--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float speed;
-    private Rigidbody playerRb;
     public float horizontalInput;
-    public float xRange = 10.0f;
-    
+    public float verticalInput;
+    public float speed = 5.0f;
+    public float xRange = 20.0f;
+    private Rigidbody playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,12 +28,10 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow)
-        {
-          
-        }
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        transform.Translate(-Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(-Vector3.forward * verticalInput * Time.deltaTime * speed);
     }
 }
